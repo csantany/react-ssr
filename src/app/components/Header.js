94855e11d51a29b671c32103678192cf64c66fca
Carlos Santana <csantana@beachbody.com>
2017-08-13 17:52:00 -0700

    Moving server.js to src/server/index.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default () => (
   <ul>
-    <li><Link to='/'>Home1</Link></li>
+    <li><Link to='/'>Home</Link></li>
     <li><Link to='/about'>About</Link></li>
     <li><Link to='/topics'>Topics</Link></li>
   </ul>
--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,8 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const webpackHotServerMiddleware = require('webpack-hot-server-middleware');
-const config = require('./webpack.config');
-const api = require('./src/server/api');
+const config = require('../../webpack.config');
+const api = require('./api');
 
 const app = express();
 const compiler = webpack(config);
